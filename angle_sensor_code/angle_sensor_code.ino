0d20ed2485b2f4f8397200314cba4909c40b0fb1
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2021-08-22 15:22:54 +1000

    the working code

--- angle_sensor_code/angle_sensor_code.ino
+++ angle_sensor_code/angle_sensor_code.ino
@@ -3,18 +3,16 @@
 #include "mcp2515_can.h"
 
 const int SPI_CS_PIN = 17;
-const int CAN_INT_PIN = 2;
 
 mcp2515_can CAN(SPI_CS_PIN);
 
 //ANGSENSOR
-#define EncoderCS1 10
 int32_t encoder1Reading = 0;
 int32_t lastencoder1Reading = 0;
 int32_t rate = 0;
-Encoder_Buffer Encoder1(EncoderCS1);
+Encoder_Buffer Encoder1(10);
 
-unsigned char buff[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+unsigned char SensorMsg[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 void setup() {
   CAN.begin(CAN_500KBPS);
@@ -25,24 +23,24 @@ void setup() {
 
 void loop() {
   //ANGSENSOR
-  encoder1Reading = Encoder1.readEncoder(); //READ the ANGSENSOR
+  encoder1Reading = Encoder1.readEncoder() * 253; //READ the ANGSENSOR
 
   rate = abs(encoder1Reading) - abs(lastencoder1Reading);
 
   lastencoder1Reading = encoder1Reading;
 
-  buff[0] = (encoder1Reading >> 24) & 0xFF; //Bitshift the ANGSENSOR (Cabana errors with 32 bit)
-  buff[1] = (encoder1Reading >> 16) & 0xFF;
-  buff[2] = (encoder1Reading >> 8) & 0xFF;
-  buff[3] = (encoder1Reading >> 0) & 0xFF;
-  buff[4] = (rate >> 16) & 0xFF;
-  buff[5] = (rate >> 8) & 0xFF;
-  buff[6] = (rate >> 0) & 0xFF;
-  buff[7] = can_cksum (buff, 7, 0x230); //Toyota CAN CHECKSUM
+  SensorMsg[0] = (encoder1Reading >> 24) & 0xFF; //Bitshift the ANGSENSOR (Cabana errors with 32 bit)
+  SensorMsg[1] = (encoder1Reading >> 16) & 0xFF;
+  SensorMsg[2] = (encoder1Reading >> 8) & 0xFF;
+  SensorMsg[3] = (encoder1Reading >> 0) & 0xFF;
+  SensorMsg[4] = (rate >> 16) & 0xFF;
+  SensorMsg[5] = (rate >> 8) & 0xFF;
+  SensorMsg[6] = (rate >> 0) & 0xFF;
+  SensorMsg[7] = can_cksum (SensorMsg, 7, 0x230); //Toyota CAN CHECKSUM
 
-  CAN.sendMsgBuf(0x23, 0, 8, buff);
+  CAN.sendMsgBuf(0x23, 0, 8, SensorMsg);
   
-  delay(10);
+  delay(125/11);
 }
 
 //TOYOTA CAN CHECKSUM
