02a4612b2d5f9b5653a8083faabe47308cc750ea
Irene <12470297+cydia2020@users.noreply.github.com>
2024-10-02 22:12:05 +1000

    finally

--- toyota-add-on-angle-sensor.ino
+++ toyota-add-on-angle-sensor.ino
@@ -3,51 +3,50 @@
 #include "mcp2515_can.h"
 #include "mcp_can.h"
 #define CAN_2515
-#define TARGET_ID 0x23
 
-const int CAN_CS_PIN = 17;
-const int ENC_CS_PIN = 10;
+const int CAN_CS_PIN = D1;
+const int ENC_CS_PIN = D2;
+const unsigned long messageInterval = 12;
+unsigned long lastCanSendTime = 0;
+int32_t encoderReading = 0;
+int32_t lastEncoderReading = 0;
+int32_t rate = 0;
+byte SensorMsg[] = {};
 
 mcp2515_can CAN(CAN_CS_PIN);
 Encoder_Buffer Encoder(ENC_CS_PIN);
 
-int32_t encoderReading = 0;
-
-unsigned char SensorMsg[8] = {};
-
 void setup() {
-  Serial.begin(115200);
-  if (CAN.begin(CAN_500KBPS) == CAN_OK) {
-    Serial.println("CAN OK");
-  } else {
-    Serial.println("CAN FUCKED");
-  }
-
+  CAN.begin(CAN_500KBPS, MCP_8MHz);
+  CAN.setMode(MODE_NORMAL);
   Encoder.initEncoder();
 }
 
 void loop() {
   // parse sensor
-  encoderReading = (Encoder.readEncoder());  //* 253.7);
+  encoderReading = Encoder.readEncoder();  //* 253.7);
+
+  rate = abs(encoderReading) - abs(lastEncoderReading);
+  lastEncoderReading = encoderReading;
+
   // parse CAN message
   SensorMsg[0] = (encoderReading >> 24) & 0xFF;
   SensorMsg[1] = (encoderReading >> 16) & 0xFF;
   SensorMsg[2] = (encoderReading >> 8) & 0xFF;
   SensorMsg[3] = (encoderReading >> 0) & 0xFF;
-  SensorMsg[7] = can_cksum(SensorMsg, 4, TARGET_ID);
+  SensorMsg[4] = (rate >> 16) & 0xFF;
+  SensorMsg[5] = (rate >> 8) & 0xFF;
+  SensorMsg[6] = (rate >> 0) & 0xFF;
+  SensorMsg[7] = toyota_checksum(SensorMsg, 7, 0x23);  //Toyota CAN CHECKSUM
 
-  if (millis() % 12 == 0) {
-    if (CAN.sendMsgBuf(TARGET_ID, 0, 5, SensorMsg) == CAN_OK) {
-      Serial.print(millis());
-      Serial.print(" at ");
-      Serial.print(encoderReading);
-      Serial.print('\n');
-    }
+  if (millis() - lastCanSendTime >= messageInterval) {
+    lastCanSendTime = millis();
+    CAN.sendMsgBuf(0x23, 0, 8, SensorMsg);
   }
 }
 
 // TOYOTA CAN CHECKSUM
-int can_cksum(uint8_t* dat, uint8_t len, uint16_t addr) {
+int toyota_checksum(uint8_t* dat, uint8_t len, uint16_t addr) {
   uint8_t checksum = 0;
   checksum = ((addr & 0xFF00) >> 8) + (addr & 0x00FF) + len + 1;
   //uint16_t temp_msg = msg;
